5ca58bc1c20e29aa768a32f7c6d995d04004488d
Alessandro Piras <laynor@gmail.com>
2012-02-03 16:29:31 +0100

    notification face change after timeout

--- emacs/enotify-espectator.el
+++ emacs/enotify-espectator.el
@@ -1,10 +1,33 @@
 ;;;; Espectator plugin for enotify
 (require 'enotify)
 
+(defgroup enotify-espectator nil
+  "Enotify plugin for espectator"
+  :group 'enotify)
+
+(defcustom enotify-espectator-change-face-timeout nil
+  "amount of seconds after which the notification face should be changed."
+  :group 'enotify-espectator
+  :type '(choice (const nil) integer))
+
+(defcustom enotify-espectator-timeout-face :standard
+  "face to apply to the notification text after
+  `enotify-espectator-change-face-timeout' seconds passed"
+  :group 'enotify-espectator
+  :type '(choice (const :tag "Standard enotify face" :standard )
+		 (const :tag "Standard enotify success face" :success)
+		 (const :tag "Standard enotify warning face" :warning)
+		 (const :tag "Standard enotify failure face" :failure)
+		 face))
+
 (defun enotify-rspec-result-buffer-name (id)
   (format "*RSpec Results: %s*" id))
 
 (defun enotify-rspec-result-message-handler (id data)
+  (when enotify-espectator-change-face-timeout
+    (run-with-timer enotify-espectator-change-face-timeout nil
+		    'enotify-change-notification-face
+		    id enotify-espectator-timeout-face))
   (let ((buf (get-buffer-create (enotify-rspec-result-buffer-name id))))
     (save-current-buffer
       (set-buffer buf)
@@ -12,8 +35,6 @@
       (insert data)
       (flet ((message (&rest args) (apply 'format args)))
 	(org-mode)))))
-      
-
 
 (defvar enotify-rspec-result-message-handler 'enotify-rspec-result-message-handler)
 
@@ -22,6 +43,7 @@
   (switch-to-buffer-other-window
    (enotify-rspec-result-buffer-name
     (enotify-event->slot-id event))))
+
 
 (defvar enotify-rspec-mouse-1-handler 'enotify-rspec-mouse-1-handler)
 
